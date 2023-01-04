67aa0d25437b31ae5df336a086fdcdeae4014d99
webiots <92780650+bhaveshborgoankar@users.noreply.github.com>
2023-01-04 10:08:39 +0530

    Update tag.middleware.js

--- middleware/tag.middleware.js
+++ middleware/tag.middleware.js
@@ -7,7 +7,7 @@ export const TagMiddleware = {
         const createRules = {
             "name": "required|string",
             "type": "required|string",
-            "is_active": "required|string"
+            "is_active": "required|boolean"
         };
 
         return ResponseValidator(req, createRules, res, next);
